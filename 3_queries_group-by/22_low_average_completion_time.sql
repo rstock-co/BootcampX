9382a4e8478c257da0a5f5e5f79a2cec96b69fb4
Richard Stock <rstock.co@gmail.com>
2022-09-19 19:16:51 -0600

    Add BootcampX Queries II exercise

---
+++ 3_queries_group-by/22_low_average_completion_time.sql
@@ -0,0 +1,8 @@
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions on student_id = students.id
+JOIN assignments on assignments.id = assignment_submissions.assignment_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
