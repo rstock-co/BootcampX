fb8eb4623aa09f4f52e7be8265d6046736adf97b
Richard Stock <rstock.co@gmail.com>
2022-09-19 18:07:16 -0600

    Add JOINS exercise

---
+++ 2_joins/12_rollover_students.sql
@@ -0,0 +1,3 @@
+SELECT students.name, cohorts.name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+FROM students JOIN cohorts ON cohort_id = cohorts.id
+WHERE students.start_date != cohorts.start_date;
